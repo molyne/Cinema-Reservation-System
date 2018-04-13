bdd1875865211c427dd5df35a252a452315dbc18
Molyn Eriksson <molyneriksson@gmail.com>
2018-04-13 20:34:42 +0200

    Initial-comit

---
+++ testMapMVC/testMapMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testMapMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
